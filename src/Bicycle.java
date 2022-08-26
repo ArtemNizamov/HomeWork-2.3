de027248a40152c70b3b687cc20e22fa4a9de455
ArtemNizamov <nizamov.artem2016@gmail.com>
2022-08-26 10:14:16 +0300

    Сдаю Домашнее задание!

---
+++ src/Bicycle.java
@@ -0,0 +1,24 @@
+public class Bicycle extends Transport implements Wheelable {
+
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку у велосипеда");
+    }
+
+    @Override
+    public void checkWheels() {
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+    }
+
+    @Override
+    public void service() {
+        System.out.println("Обслуживаем велосипед " + getModelName());
+        checkWheels();
+    }
+}
---
+++ src/Wheelable.java
@@ -0,0 +1,5 @@
+public interface Wheelable {
+    void checkWheels();
+
+    void updateTyre();
+}
